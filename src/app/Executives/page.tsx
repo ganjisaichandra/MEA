4c6ba9bdfde65d2d4ef01f783218b75e3c4590d5
asmdnayeeem <asmdnayeem245@gmail.com>
2024-09-11 22:33:03 +0530

    executive page done

--- src/app/Executives/page.tsx
+++ src/app/Executives/page.tsx
@@ -1,5 +1,130 @@
+import im from "../../assets/images/Ellipse 1.png";
 import React from "react";
+import Image, { StaticImageData } from "next/image";
+type secretory = {
+  name: string;
+  role: string;
+  image: StaticImageData | string;
+}[];
+type cosecretory = {
+  name: string;
+  role: string;
+  image: StaticImageData | string;
+}[];
+type executives = {
+  name: string;
+  role: string;
+  image: StaticImageData | string;
+}[];
+const sec: secretory = [
+  {
+    name: "NA",
+    role: "secretory",
+    image: im,
+  },
+  {
+    name: "NA",
+    role: "secretory",
+    image: im,
+  },
+];
+const cosec: cosecretory = [
+  {
+    name: "NA",
+    role: "co-secretory",
+    image: im,
+  },
+];
+const exec: executives = [
+  {
+    name: "NA",
+    role: "executive",
+    image: im,
+  },
+  {
+    name: "NA",
+    role: "executive",
+    image: im,
+  },
+];
+const page = () => {
+  return (
+    <div className=" min-h-screen relative p-4 mb-20">
+      <div className="flex  min-h-screen w-full relative justify-center ">
+        <div className="flex justify-around items-center w-full flex-wrap gap-8 ">
+          {sec.map((exe, index) => (
+            <div
+              key={index}
+              className=" p-6 w-[30rem] min-w-max rounded-lg flex justify-around items-center shadow-xl bg-blue-900 transform hover:-translate-y-2 transition-all duration-300"
+            >
+              <Image
+                src={exe.image}
+                alt={exe.name}
+                width={12}
+                height={12}
+                className="w-32 h-32 object-cover rounded-full mx-auto mb-4 border-4 border-black"
+              />
+              <div className="w-full">
+                <h3 className="text-lg font-semibold mb-2 text-center text-white">
+                  {exe.name}
+                </h3>
+                <p className="text-xl font-metrapolis-bold mb-4 text-center text-white">
+                  {exe.role}
+                </p>
+              </div>
+            </div>
+          ))}
 
-export default function page() {
-  return <div>page</div>;
-}
+          <div className="flex justify-around items-center w-full flex-wrap gap-8 ">
+            {cosec.map((exe, index) => (
+              <div
+                key={index}
+                className=" p-6 w-[30rem] min-w-max rounded-lg flex justify-around items-center shadow-xl bg-blue-900 transform hover:-translate-y-2 transition-all duration-300"
+              >
+                <Image
+                  src={exe.image}
+                  alt={exe.name}
+                  width={12}
+                  height={12}
+                  className="w-32 h-32 object-cover rounded-full mx-auto mb-4 border-4 border-black"
+                />
+                <div className="w-full">
+                  <h3 className="text-lg font-semibold mb-2 text-center text-white">
+                    {exe.name}
+                  </h3>
+                  <p className="text-xl font-metrapolis-bold mb-4 text-center text-white">
+                    {exe.role}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="flex justify-around items-center w-full flex-wrap gap-8 ">
+            {exec.map((exe, index) => (
+              <div
+                key={index}
+                className=" p-6 w-[30rem] min-w-max rounded-lg flex flex-col justify-around items-center shadow-xl bg-blue-900 transform hover:-translate-y-2 transition-all duration-300"
+              >
+                <Image
+                  src={exe.image}
+                  alt={exe.name}
+                  width={12}
+                  height={12}
+                  className="w-32 h-32 object-cover rounded-full mx-auto mb-4 border-4 border-black"
+                />
+                <h3 className="text-lg font-semibold mb-2 text-center text-white">
+                  {exe.name}
+                </h3>
+                <p className="text-xl font-metrapolis-bold mb-4 text-center text-white">
+                  {exe.role}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default page;
--- src/app/Magazine/page.tsx
+++ src/app/Magazine/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className="font-metrapolis-bold text-[2rem] h-screen w-full flex justify-center items-center">
+      Coming Soon....
+    </div>
+  );
 }
